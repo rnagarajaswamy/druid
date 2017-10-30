c53bbb4eaf31aa9e76963f3aa8366b53a10e8cdb
Raghavendra Nagarajaswamy <Raghavendra.Nagarajaswamy@pacer.com>
2017-10-30 15:01:09 -0400

    connection string to druid broker

---
+++ 001_basic_connection_check.py
@@ -0,0 +1,16 @@
+import pydruid
+from pylab import plt
+
+query = PyDruid('http://10.107.1.31:8082', 'druid/v2')
+
+# Other packages and modules
+#from pydruid import *
+#from pydruid.client import *
+#from pylab import plt
+#from pydruid.query import QueryBuilder
+#from pydruid.utils.postaggregator import *
+#from pydruid.utils.aggregators import *
+#from pydruid.utils.filters import *
+#from pydruid.client import *
+#from pylab import plt
+
