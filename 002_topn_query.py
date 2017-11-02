a2756e5ae6107628f29d94e8dbbbcd3dabbe8f1f
rnagarajaswamy <raghavendra.nagarajaswamy@xpo.com>
2017-11-01 21:23:47 -0400

    added exception handling

--- 002_topn_query.py
+++ 002_topn_query.py
@@ -1,4 +1,4 @@
-# importing packages
+## importing packages
 from pydruid import *
 from pydruid.client import *
 from pylab import plt
@@ -7,30 +7,30 @@ from pydruid.utils.postaggregator import *
 from pydruid.utils.aggregators import *
 from pydruid.utils.filters import *
 from pydruid.client import *
-from pylab import plt
 
 # connect to druid broker node
 #query = PyDruid('http://10.107.1.31:8082', 'druid/v2')
 query = PyDruid('http://192.168.8.104:8082', 'druid/v2')
 
 #constricting query
-top = query.topn(
-    datasource='wikiticker',
-    intervals='2015-09-12/2015-09-13',
-    granularity='all',
-    dimension='page',
-    metric='edits',
-    threshold=25,
-    aggregations={'edits': longsum('count')}
-    
-    # filers
-    #filter=(Dimension('user_lang') == 'en') & (Dimension('first_hashtag') == 'oscars') &
-    #       (Dimension('user_time_zone') == 'Pacific Time (US & Canada)') &
-    #       ~(Dimension('user_mention_name') == 'No Mention'),
-    
-    
+try:
+	top = query.topn(
+		datasource='wikiticker',
+		intervals='2015-09-12/2015-09-13',
+		granularity='all',
+		dimension='page',
+		metric='edits',
+		threshold=25,
+		aggregations={'edits': longsum('count')}
+		
+		# filers
+		#filter=(Dimension('user_lang') == 'en') & (Dimension('first_hashtag') == 'oscars') &
+		#       (Dimension('user_time_zone') == 'Pacific Time (US & Canada)') &
+		#       ~(Dimension('user_mention_name') == 'No Mention'),
 )
+except Exception as e:
+	print("Error {0}".format(e))
 
-# storing the output in object
+	# storing the output in object
 df = query.export_pandas()
 print(df)
