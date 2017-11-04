dce843c0f729158f164b394f1b42133627b12a59
rnagarajaswamy <raghavendra.nagarajaswamy@xpo.com>
2017-11-04 13:09:37 -0400

    code that constructs the JSON query

---
+++ 010_get_json_fromat_of_query.py
@@ -0,0 +1,59 @@
+## importing library
+from pydruid import *
+from pydruid.client import *
+from pydruid.query import QueryBuilder
+from pydruid.utils.postaggregator import *
+from pydruid.utils.aggregators import *
+from pydruid.utils.filters import *
+from pydruid.client import *
+
+# code block starts here 
+try:
+    # connecting to DRUID
+    query = PyDruid('http://192.168.8.105:8082', 'druid/v2')
+    #client = PyDruid('http://10.107.1.124:8082', 'druid/v2')
+    
+    # defining the query object and rules
+    top = query.topn(
+                datasource="wikiticker"
+                , granularity="hour"
+                , intervals=["2015-09-12/2015-09-13"]
+                , dimension="regionName"
+                , filter=~(Dimension("regionName") == None)
+                , aggregations={"edits":longsum("count")}
+                , metric="edits"
+                , threshold=1
+             )  
+
+    print(json.dumps(top.query_dict, indent=2))  
+
+except Exception as e:
+    print("Error {0}".format(e))
+
+'''output
+{
+  "queryType": "topN",
+  "dataSource": "wikiticker",
+  "granularity": "hour",
+  "intervals": [
+    "2015-09-12/2015-09-13"
+  ],
+  "dimension": "regionName",
+  "filter": {
+    "type": "not",
+    "field": {
+      "type": "selector",
+      "dimension": "regionName",
+      "value": null
+    }
+  },
+  "aggregations": [
+    {
+      "type": "longSum",
+      "fieldName": "count",
+      "name": "edits"
+    }
+  ],
+  "metric": "edits",
+  "threshold": 1
+}  '''
